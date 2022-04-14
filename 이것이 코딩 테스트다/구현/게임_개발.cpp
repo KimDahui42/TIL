937f88dc5b0c121657f5d1089b4c497ae68137b3
KimDahui42 <monu00424@sookmyung.ac.kr>
2022-04-15 02:11:19 +0900

    코테-상하좌우

---
+++ 이것이 코딩 테스트다/구현/게임_개발.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+/*
+현민이는 게임 캐릭터가 맵 안에서 움직이는 시스템을 개발 중이다. 캐릭터가 있는 장소는 1*1 크기의 정사각형으로 이뤄진 n*m크기의 직사각형으로,
+각각의 칸은 육지 또는 바다이다. 캐릭터는 동서남북 중 한 곳을 바라본다.
+맵의 각 칸으 ㄴ(A,B)로 나타낼 수 있고, A는 북쪽으로부터 떨어진 칸의 개스
+*/
+int main(){
+    int n,m,a,b,d;
+    cin>>n>>m;
+    cin>>a>>b>>d;
+    vector<vector<bool>>v(n,vector<bool>(m,false));
+    while(true){
+        
+    }
+
+}
